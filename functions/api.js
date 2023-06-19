15472904fb3b61c52f4dc01008ef30c254766c83
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-06-19 16:43:19 +0700

    set large request entity

--- functions/api.js
+++ functions/api.js
@@ -28,10 +28,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/', indexRouter);
 app.use('/', registerRouter);
